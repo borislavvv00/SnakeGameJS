767ef23ec8e928b673ec7737100291640c93a9f8
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-05-09 10:49:20 +0300

    Update Source.js

--- Source.js
+++ Source.js
@@ -3,6 +3,26 @@
  	canvas = document.getElementById("map");
 	context = canvas.getContext("2d");
 	document.addEventListener("keydown", GetKeyboardCommand);
+	var btn = document.getElementById("walls");
+	var btnClick = 0;
+	btn.onclick = function ()
+	{
+		if(btnClick % 2 == 0)
+		{
+			btn.innerHTML = "wallsOn";
+			Snake.direction = "stop";
+			create = true;
+			drawWall = true;
+		}
+		else
+		{
+			btn.innerHTML = "wallsOff";
+			Snake.direction = "stop";
+			create = false;
+			drawWall = false;
+		}
+		btnClick++;
+	}
 	if(gameOver == false)
 	{
 		setInterval(DrawGameElements,1);
