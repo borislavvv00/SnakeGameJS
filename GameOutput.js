a86df50b1c676fd3d1f408cdf46cca46814d74d1
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-06-04 22:18:22 +0300

    Update and rename OutputGameElements.js to GameOutput.js

--- GameOutput.js
+++ GameOutput.js
@@ -1,11 +1,11 @@
-var gameOver = false;
-var timeBeforeSupperFood = 5;//count befor supper food
-var score = 0;
-var timeForSupperFood = false;//if now is supper food draw
+var isGameOver = false;
+var isTimeForSupperFood = false;//if now is supper food draw
+var isWallCreate = false;
+var isWallDrawed = false;
 var frameCount = 0;
 var frames = 1;
-var create = false;
-var drawWall = false;
+var timeBeforeSupperFood = 5;//count befor supper food
+var score = 0;
 
 function Draw(context, color, y, x, ySize, xSize) //Draw an element
 {
@@ -40,29 +40,47 @@ function DrawWalls()
 	}
 }
 
+function DrawGameOver()
+{
+	context.font = "30px Arial";
+	context.fillStyle = "white";
+	context.fillText("GAME OVER ",map.height / 2 - 100, map.width / 2 - 100);
+}
+
 function DrawGameElements()
 { 
-	if(create == true)
-	{	
-		CreateWall();
-		create = false;
-	}
 	frameCount++;
 	if(frameCount == frames)//push new tail part coordinates every 21 frames
 	{	
 		Snake.tail.push({x :Snake.headX, y : Snake.headY });
 		frames += 21;
 	}
+//----------------------------Draw functions-----------------------------------------	
 	Draw(context, "black", 0, 0, map.width, map.height);
 	DrawSnakeParts();
-	if(drawWall == true)
+	
+	if(isWallCreate == true)
+	{	
+		CreateWall();
+		isWallCreate = false;
+	}
+	
+	if(isWallDrawed == true)
 	{	
 		DrawWalls();
 	}
-	Direction();
-	TouchMapBorder();
-	TouchWall();
 	ChooseFoodToDraw();
-	//GameOver();
+//---------------------------Game logic functions-------------------------------------
+	if(isGameOver == false)
+	{	
+		Direction();
+		TouchMapBorder();
+		TouchWall();
+		GameOver();
+	}
+	else
+	{
+		DrawGameOver();
+	}
 	document.getElementById("score").innerHTML = "score = " + score;
 }
